50e61d5e50cdc7f6dcc53d4e52832403b735a994
h662 <h662hong@gmail.com>
2024-05-15 11:47:05 +0900

    remove supabaseClient

--- vite/src/components/Layout.tsx
+++ vite/src/components/Layout.tsx
@@ -2,42 +2,13 @@ import { FC, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Session } from "@supabase/supabase-js";
-import { supabaseClient } from "../lib/supabaseClient";
-
-export interface OutletContext {
-  session: Session | null;
-}
 
 const Layout: FC = () => {
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    supabaseClient.auth.getSession().then(({ data }) => {
-      setSession(data.session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabaseClient.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
-
-  useEffect(() => {
-    if (!session) return;
-
-    // test
-    console.log(session);
-  }, [session]);
-
   return (
     <>
       <Header />
       <main>
-        <Outlet context={{ session }} />
+        <Outlet />
       </main>
       <Footer />
     </>
